1da01ca543f29bd7f56ff4e8e88402d7865d5c45
DIXANT SONI <dixantofficial@gmail.com>
2026-09-01 03:33:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import { PipelineToolbar } from './toolbar';
-import { PipelineUI } from './ui';
-import { SubmitButton } from './submit';
+import { PipelineToolbar } from "./toolbar";
+import { PipelineUI } from "./ui";
+import { SubmitButton } from "./submit";
+import ExecutionPanel from "./components/ExecutionPanel";
 
 function App() {
   return (
     <div>
-    
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
-    
+      <ExecutionPanel />
     </div>
   );
 }
